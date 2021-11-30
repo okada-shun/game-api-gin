4b06ae0da834b133cf58de1baaec2d157df82857
okada-shun <syun19981207@gmail.com>
2021-11-30 14:51:21 +0900

    Fix api test code files

--- api/gacha_test.go
+++ api/gacha_test.go
@@ -11,6 +11,9 @@ import (
 	"game-api-gin/config"
 	"game-api-gin/database"
 	"game-api-gin/gmtoken"
+	"game-api-gin/model"
+	"game-api-gin/test"
+	"game-api-gin/util"
 
 	"github.com/gin-gonic/gin"
 	"github.com/stretchr/testify/assert"
@@ -23,7 +26,6 @@ func TestGachaSuite(t *testing.T) {
 
 type GachaSuite struct {
 	suite.Suite
-	uapi       *UserAPI
 	gapi       *GachaAPI
 	auth       *auth.Auth
 	db         *database.GormDatabase
@@ -31,7 +33,8 @@ type GachaSuite struct {
 	ctx        *gin.Context
 	recorder   *httptest.ResponseRecorder
 	token      string
-	privatekey string
+	privateKey string
+	userId     string
 }
 
 func (s *GachaSuite) BeforeTest(string, string) {
@@ -47,8 +50,20 @@ func (s *GachaSuite) BeforeTest(string, string) {
 	t, err := gmtoken.NewGmtokenTx(config)
 	assert.Nil(s.T(), err)
 	s.tx = t
-	s.uapi = &UserAPI{Auth: s.auth, DB: s.db, Tx: s.tx}
 	s.gapi = &GachaAPI{Auth: s.auth, DB: s.db, Tx: s.tx}
+
+	userId, err := util.CreateUUId()
+	assert.Nil(s.T(), err)
+	s.userId = userId
+	privateKey, err := util.CreatePrivateKey()
+	assert.Nil(s.T(), err)
+	s.privateKey = privateKey
+	s.db.CreateUser(model.User{UserID: userId, Name: "mike", PrivateKey: privateKey})
+	token, err := s.auth.CreateToken(userId)
+	assert.Nil(s.T(), err)
+	s.token = token
+	s.gapi.Tx.MintGmtoken(100, privateKey)
+
 	s.T().Log("BeforeTest!!")
 }
 
@@ -57,26 +72,8 @@ func (s *GachaSuite) AfterTest(suiteName, testName string) {
 	s.T().Log("AfterTest!!")
 }
 
-func (s *GachaSuite) SetTokenKey() {
-	s.ctx.Request = httptest.NewRequest("POST", "/user/create", strings.NewReader(`{"name":"satou"}`))
-	s.ctx.Request.Header.Set("Content-Type", "application/json")
-	s.uapi.CreateUser(s.ctx)
-	body, _ := ioutil.ReadAll(s.recorder.Body)
-	var createUserResponse CreateUserResponse
-	json.Unmarshal(body, &createUserResponse)
-	s.token = createUserResponse.Token
-	s.ctx.Request.Header.Set("x-token", s.token)
-
-	userId, err := s.auth.GetUserId(s.ctx)
-	assert.Nil(s.T(), err)
-	user, err := s.db.GetUser(userId)
-	assert.Nil(s.T(), err)
-	s.privatekey = user.PrivateKey
-}
-
 func (s *GachaSuite) Test_DrawGacha() {
-	s.SetTokenKey()
-	s.gapi.Tx.TransferEth(200000000000000000, s.privatekey)
+	s.gapi.Tx.TransferEth(200000000000000000, s.privateKey)
 
 	s.ctx.Request = httptest.NewRequest("POST", "/gacha/draw", strings.NewReader(`{"gacha_id":1,"times":10}`))
 	s.ctx.Request.Header.Set("Content-Type", "application/json")
@@ -85,21 +82,32 @@ func (s *GachaSuite) Test_DrawGacha() {
 	s.gapi.DrawGacha(s.ctx)
 
 	assert.Equal(s.T(), 200, s.recorder.Code)
+
 	body, _ := ioutil.ReadAll(s.recorder.Body)
 	var drawGachaResponse DrawGachaResponse
 	json.Unmarshal(body, &drawGachaResponse)
 	assert.Equal(s.T(), 10, len(drawGachaResponse.Results))
-
-	s.ctx.Request = httptest.NewRequest("GET", "/user/get", nil)
-	s.ctx.Request.Header.Set("Content-Type", "application/json")
-	s.ctx.Request.Header.Set("x-token", s.token)
-
-	s.uapi.GetUser(s.ctx)
-
-	body, _ = ioutil.ReadAll(s.recorder.Body)
-	var getUserResponse GetUserResponse
-	json.Unmarshal(body, &getUserResponse)
-	assert.Equal(s.T(), 90, getUserResponse.GmtokenBalance)
+	var gachaResults []model.GachaResult
+	for i := 1; i < 10; i++ {
+		gachaResults = append(gachaResults, drawGachaResponse.Results[i])
+	}
+	var characterIds []string
+	s.db.DB.Table("gacha_characters").Select("gacha_character_id").Where("gacha_id = ?", 1).Scan(&characterIds)
+	for _, v := range gachaResults {
+		contains := test.Contains(characterIds, v.CharacterID)
+		assert.Equal(s.T(), true, contains)
+	}
+	var characterNames []string
+	s.db.DB.Table("gacha_characters").Select("characters.character_name").
+		Joins("join characters on gacha_characters.character_id = characters.id").
+		Where("gacha_id = ?", 1).Scan(&characterNames)
+	for _, v := range gachaResults {
+		contains := test.Contains(characterNames, v.Name)
+		assert.Equal(s.T(), true, contains)
+	}
+	_, bal, err := s.tx.GetAddressBalance(s.privateKey)
+	assert.Nil(s.T(), err)
+	assert.Equal(s.T(), 90, bal)
 }
 
 func (s *GachaSuite) Test_DrawGacha_WithoutEnoughEth() {
@@ -113,9 +121,11 @@ func (s *GachaSuite) Test_DrawGacha_WithoutEnoughEth() {
 }
 
 func (s *GachaSuite) Test_DrawGacha_ByInvalidToken() {
+	s.gapi.Tx.TransferEth(200000000000000000, s.privateKey)
+
 	s.ctx.Request = httptest.NewRequest("POST", "/gacha/draw", strings.NewReader(`{"gacha_id":1,"times":10}`))
 	s.ctx.Request.Header.Set("Content-Type", "application/json")
-	s.ctx.Request.Header.Set("x-token", "InvalidToken")
+	s.ctx.Request.Header.Set("x-token", s.token+"InvalidToken")
 
 	s.gapi.DrawGacha(s.ctx)
 
@@ -123,6 +133,8 @@ func (s *GachaSuite) Test_DrawGacha_ByInvalidToken() {
 }
 
 func (s *GachaSuite) Test_DrawGacha_ByInvalidGachaID() {
+	s.gapi.Tx.TransferEth(200000000000000000, s.privateKey)
+
 	s.ctx.Request = httptest.NewRequest("POST", "/gacha/draw", strings.NewReader(`{"gacha_id":10,"times":10}`))
 	s.ctx.Request.Header.Set("Content-Type", "application/json")
 	s.ctx.Request.Header.Set("x-token", s.token)
@@ -133,6 +145,8 @@ func (s *GachaSuite) Test_DrawGacha_ByInvalidGachaID() {
 }
 
 func (s *GachaSuite) Test_DrawGacha_ByZeroTimes() {
+	s.gapi.Tx.TransferEth(200000000000000000, s.privateKey)
+
 	s.ctx.Request = httptest.NewRequest("POST", "/gacha/draw", strings.NewReader(`{"gacha_id":1,"times":0}`))
 	s.ctx.Request.Header.Set("Content-Type", "application/json")
 	s.ctx.Request.Header.Set("x-token", s.token)
@@ -143,6 +157,8 @@ func (s *GachaSuite) Test_DrawGacha_ByZeroTimes() {
 }
 
 func (s *GachaSuite) Test_DrawGacha_ByMinusTimes() {
+	s.gapi.Tx.TransferEth(200000000000000000, s.privateKey)
+
 	s.ctx.Request = httptest.NewRequest("POST", "/gacha/draw", strings.NewReader(`{"gacha_id":1,"times":-10}`))
 	s.ctx.Request.Header.Set("Content-Type", "application/json")
 	s.ctx.Request.Header.Set("x-token", s.token)
@@ -153,6 +169,8 @@ func (s *GachaSuite) Test_DrawGacha_ByMinusTimes() {
 }
 
 func (s *GachaSuite) Test_DrawGacha_ByExcessiveTimes() {
+	s.gapi.Tx.TransferEth(200000000000000000, s.privateKey)
+
 	s.ctx.Request = httptest.NewRequest("POST", "/gacha/draw", strings.NewReader(`{"gacha_id":1,"times":1000}`))
 	s.ctx.Request.Header.Set("Content-Type", "application/json")
 	s.ctx.Request.Header.Set("x-token", s.token)
--- api/user_test.go
+++ api/user_test.go
@@ -11,7 +11,10 @@ import (
 	"game-api-gin/config"
 	"game-api-gin/database"
 	"game-api-gin/gmtoken"
+	"game-api-gin/model"
+	"game-api-gin/util"
 
+	"github.com/dgrijalva/jwt-go"
 	"github.com/gin-gonic/gin"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/suite"
@@ -23,13 +26,15 @@ func TestUserSuite(t *testing.T) {
 
 type UserSuite struct {
 	suite.Suite
-	uapi     *UserAPI
-	auth     *auth.Auth
-	db       *database.GormDatabase
-	tx       *gmtoken.GmtokenTx
-	ctx      *gin.Context
-	recorder *httptest.ResponseRecorder
-	token    string
+	uapi       *UserAPI
+	auth       *auth.Auth
+	db         *database.GormDatabase
+	tx         *gmtoken.GmtokenTx
+	ctx        *gin.Context
+	recorder   *httptest.ResponseRecorder
+	token      string
+	privateKey string
+	userId     string
 }
 
 func (s *UserSuite) BeforeTest(string, string) {
@@ -46,6 +51,18 @@ func (s *UserSuite) BeforeTest(string, string) {
 	assert.Nil(s.T(), err)
 	s.tx = t
 	s.uapi = &UserAPI{Auth: s.auth, DB: s.db, Tx: s.tx}
+
+	userId, err := util.CreateUUId()
+	assert.Nil(s.T(), err)
+	s.userId = userId
+	privateKey, err := util.CreatePrivateKey()
+	assert.Nil(s.T(), err)
+	s.privateKey = privateKey
+	s.db.CreateUser(model.User{UserID: userId, Name: "mike", PrivateKey: privateKey})
+	token, err := s.auth.CreateToken(userId)
+	assert.Nil(s.T(), err)
+	s.token = token
+
 	s.T().Log("BeforeTest!!")
 }
 
@@ -66,6 +83,19 @@ func (s *UserSuite) Test_CreateUser() {
 	s.uapi.CreateUser(s.ctx)
 
 	assert.Equal(s.T(), 200, s.recorder.Code)
+
+	body, _ := ioutil.ReadAll(s.recorder.Body)
+	var createUserResponse CreateUserResponse
+	json.Unmarshal(body, &createUserResponse)
+	token, err := s.auth.VerifyToken(createUserResponse.Token)
+	assert.Nil(s.T(), err)
+	claims := token.Claims.(jwt.MapClaims)
+	userId := claims["userId"].(string)
+	var privateKey string
+	s.db.DB.Table("users").Select("private_key").Where("user_id = ?", userId).Scan(&privateKey)
+	_, bal, err := s.tx.GetAddressBalance(privateKey)
+	assert.Nil(s.T(), err)
+	assert.Equal(s.T(), 100, bal)
 }
 
 func (s *UserSuite) Test_CreateUser_ByIntName() {
@@ -86,18 +116,7 @@ func (s *UserSuite) Test_CreateUser_ByNil() {
 	assert.Equal(s.T(), 400, s.recorder.Code)
 }
 
-func (s *UserSuite) SetToken() {
-	s.ctx.Request = httptest.NewRequest("POST", "/user/create", strings.NewReader(`{"name":"mike"}`))
-	s.ctx.Request.Header.Set("Content-Type", "application/json")
-	s.uapi.CreateUser(s.ctx)
-	body, _ := ioutil.ReadAll(s.recorder.Body)
-	var createUserResponse CreateUserResponse
-	json.Unmarshal(body, &createUserResponse)
-	s.token = createUserResponse.Token
-}
-
 func (s *UserSuite) Test_GetUser() {
-	s.SetToken()
 	s.ctx.Request = httptest.NewRequest("GET", "/user/get", nil)
 	s.ctx.Request.Header.Set("Content-Type", "application/json")
 	s.ctx.Request.Header.Set("x-token", s.token)
@@ -105,17 +124,20 @@ func (s *UserSuite) Test_GetUser() {
 	s.uapi.GetUser(s.ctx)
 
 	assert.Equal(s.T(), 200, s.recorder.Code)
+
 	body, _ := ioutil.ReadAll(s.recorder.Body)
 	var getUserResponse GetUserResponse
 	json.Unmarshal(body, &getUserResponse)
+	address, err := gmtoken.ConvertKeyToAddress(s.privateKey)
+	assert.Nil(s.T(), err)
 	assert.Equal(s.T(), "mike", getUserResponse.Name)
-	assert.Equal(s.T(), 100, getUserResponse.GmtokenBalance)
+	assert.Equal(s.T(), address.Hex(), getUserResponse.Address)
 }
 
 func (s *UserSuite) Test_GetUser_ByInvalidToken() {
 	s.ctx.Request = httptest.NewRequest("GET", "/user/get", nil)
 	s.ctx.Request.Header.Set("Content-Type", "application/json")
-	s.ctx.Request.Header.Set("x-token", "InvalidToken")
+	s.ctx.Request.Header.Set("x-token", s.token+"InvalidToken")
 
 	s.uapi.GetUser(s.ctx)
 
@@ -123,7 +145,6 @@ func (s *UserSuite) Test_GetUser_ByInvalidToken() {
 }
 
 func (s *UserSuite) Test_UpdateUser() {
-	s.SetToken()
 	s.ctx.Request = httptest.NewRequest("PUT", "/user/update", strings.NewReader(`{"name":"wang"}`))
 	s.ctx.Request.Header.Set("Content-Type", "application/json")
 	s.ctx.Request.Header.Set("x-token", s.token)
@@ -131,9 +152,7 @@ func (s *UserSuite) Test_UpdateUser() {
 	s.uapi.UpdateUser(s.ctx)
 
 	assert.Equal(s.T(), 200, s.recorder.Code)
-	userId, err := s.auth.GetUserId(s.ctx)
-	assert.Nil(s.T(), err)
-	user, err := s.db.GetUser(userId)
+	user, err := s.db.GetUser(s.userId)
 	assert.Nil(s.T(), err)
 	assert.Equal(s.T(), "wang", user.Name)
 }
@@ -141,7 +160,7 @@ func (s *UserSuite) Test_UpdateUser() {
 func (s *UserSuite) Test_UpdateUser_ByInvalidToken() {
 	s.ctx.Request = httptest.NewRequest("PUT", "/user/update", strings.NewReader(`{"name":"wang"}`))
 	s.ctx.Request.Header.Set("Content-Type", "application/json")
-	s.ctx.Request.Header.Set("x-token", "InvalidToken")
+	s.ctx.Request.Header.Set("x-token", s.token+"InvalidToken")
 
 	s.uapi.UpdateUser(s.ctx)
 
